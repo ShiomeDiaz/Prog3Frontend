1117bc30b059cf54e27e3c5aaa39f7af84176680
ShiomeDiaz <shiomediaz@gmail.com>
2020-08-01 18:21:50 -0500

    configuracion master page y routing con sus redireccionesde rutas

--- countrysideNG/src/app/app-routing.module.ts
+++ countrysideNG/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './public/home/default/default.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: DefaultComponent
+  },
+  {
+    path:'',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  
+  },
+
+  /**this must be the last one*/
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- countrysideNG/src/app/modules/security/security-routing.module.ts
+++ countrysideNG/src/app/modules/security/security-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent
+  },
+  {
+    path: 'reset',
+    component: PasswordResetComponent
+  },
+  {
+    path: 'change-password',
+    component: ChangePasswordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
